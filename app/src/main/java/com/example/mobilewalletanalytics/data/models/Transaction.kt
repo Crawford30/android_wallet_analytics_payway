269d30c3a7bd6a4fd95101b78f05af2bfa3f5411
Joel Crawford <joelcrawford30@gmail.com>
2023-11-28 13:40:30 +0300

    Parcelize depencies

---
+++ app/src/main/java/com/example/mobilewalletanalytics/data/models/Transaction.kt
@@ -0,0 +1,15 @@
+package com.example.mobilewalletanalytics.data.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Transaction(
+    val id: Int,
+    val transactionDate: String,
+    val amount: Int,
+    val type: String,
+    val service: String,
+    val category: String
+):Parcelable
