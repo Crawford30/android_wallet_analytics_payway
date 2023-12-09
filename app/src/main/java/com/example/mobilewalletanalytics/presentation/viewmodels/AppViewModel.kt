b578d8e161e53efc2d49049b20f20511c8ab120d
Crawford30 <30619160+Crawford30@users.noreply.github.com>
2023-12-09 16:17:42 +0300

    Update AppViewModel.kt

--- app/src/main/java/com/example/mobilewalletanalytics/presentation/viewmodels/AppViewModel.kt
+++ app/src/main/java/com/example/mobilewalletanalytics/presentation/viewmodels/AppViewModel.kt
@@ -17,9 +17,7 @@ import javax.inject.Inject
 @HiltViewModel
 class AppViewModel @Inject constructor(private val remoteRepo: RemoteRepo) : ViewModel() {
 
-    /**
-     * All transactions history are fetched from here. There's no pagination here since the users are few(10)
-     */
+    
     // LiveData for transactions history
     private val _transactionsHistoryLiveData = MutableLiveData<List<Transaction>>()
 
